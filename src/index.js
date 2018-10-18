87fd61da693ce19dad638cd2992f5002372cc017
Vadym Chekan <vadym.chekan@jet.com>
2018-10-18 17:21:59 -0400

    Recent elm-webpack-loader can handle elm 0.19. Fix 0.19 branch

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ require('font-awesome/css/font-awesome.css');
 // Require index.html so it gets copied to dist
 require('./index.html');
 
-var Elm = require('../elm-out/app.js');
+var Elm = require('./Main.elm');
 var mountNode = document.getElementById('main');
 
 var app = Elm.Elm.Main.init({
